f648b96b8cce76946ccaa817bc884220f04fb48a
BenyaSifiso <benyasifiso@gmail.com>
2023-06-09 05:14:10 -0700

    update

---
+++ 0x02-python-import_modules/add_0.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+def add(a, b):
+    """My addition sum function
+
+    Args:
+        a: first integer
+        b: second integer
+
+    Returns:
+        The value of return. a + b
+    """
+    return (a + b)
---
+++ 0x02-python-import_modules/calculator_1.py
@@ -0,0 +1,51 @@
+#!/usr/bin/python3
+def add(a, b):
+    """My addition sum function
+
+    Args:
+        a: first integer
+        b: second integer
+
+    Returns:
+        The value return value. a + b
+    """
+    return (a + b)
+
+
+def sub(a, b):
+    """My subtraction minus function
+
+    Args:
+        a: first integer
+        b: second integer
+
+    Returns:
+        The value for return. a - b
+    """
+    return (a - b)
+
+
+def mul(a, b):
+    """My multiplication x function
+
+    Args:
+        a: first integer
+        b: second integer
+
+    Returns:
+        The value of return. a * b
+    """
+    return (a * b)
+
+
+def div(a, b):
+    """My division / function
+
+    Args:
+        a: first integer
+        b: second integer
+
+    Returns:
+        The return value. a / b
+    """
+    return int(a / b)
